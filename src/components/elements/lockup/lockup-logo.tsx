4188a6400be746faac93bfa0317055e4f17ce504
Jen Breese-Kauth <jbreese@stanford.edu>
2024-04-11 22:20:25 +0000

    change logo based on width

--- src/components/elements/lockup/lockup-logo.tsx
+++ src/components/elements/lockup/lockup-logo.tsx
@@ -1,23 +1,21 @@
 import {Maybe} from "@lib/gql/__generated__/drupal.d";
 import PressFooterLogoLg from "@components/images/logo-lg";
-// import PressFooterLogoXs from "@components/images/logo-xs";
+import PressFooterLogoXs from "@components/images/logo-xs";
 
 const LockupLogo = ({logoUrl, siteName = ""}: { logoUrl?: Maybe<string>, siteName?: Maybe<string> }) => {
 
   return (
     <>
-      {logoUrl &&
-        <picture>
-          <img
-            src={logoUrl}
-            alt={`${siteName} Logo`}
-            className="object-contain max-w-[400px] max-h-[35px] h-auto"
-          />
-        </picture>
-      }
-      {!logoUrl &&
-        <PressFooterLogoLg className="block"/>
-      }
+      <div className="hidden sm:block">
+        {!logoUrl &&
+          <PressFooterLogoLg/>
+        }
+      </div>
+      <div className="sm:hidden">
+        {!logoUrl &&
+          <PressFooterLogoXs/>
+        }
+      </div>
     </>
   )
 }
