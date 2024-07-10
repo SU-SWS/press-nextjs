ac1b00f6c26cde9a7fcdb323f3e2c0a9948e1ff6
Mike Decker <pookmish@gmail.com>
2024-07-10 12:03:35 -0700

    Removed region detection

--- src/lib/hooks/useIsInternational.tsx
+++ src/lib/hooks/useIsInternational.tsx
@@ -1,22 +1,27 @@
 "use client"
 
-import {useBoolean, useLocalStorage} from "usehooks-ts"
-import {useCallback, useEffect} from "react"
+import {useLocalStorage} from "usehooks-ts"
+
+// Region detection is removed. Leave the hook in case it's desired in the future.
+// const useIsInternational = (): [boolean, (_value: boolean) => void] => {
+//   const {value: isIntl, setValue: setIsIntl} = useBoolean(false)
+//   const [userIntl, setUserIntl] = useLocalStorage<boolean | undefined>("intl", undefined, {initializeWithValue: false})
+//
+//   const onEvent = useCallback(
+//     ({coords}: {coords: GeolocationCoordinates}) => {
+//       const {latitude, longitude} = coords
+//       setIsIntl(latitude < 24.5 || longitude > -52 || longitude < -170)
+//     },
+//     [setIsIntl]
+//   )
+//
+//   useEffect(() => navigator.geolocation.getCurrentPosition(onEvent), [onEvent])
+//   return [userIntl !== undefined ? userIntl : isIntl, setUserIntl]
+// }
 
 const useIsInternational = (): [boolean, (_value: boolean) => void] => {
-  const {value: isIntl, setValue: setIsIntl} = useBoolean(false)
   const [userIntl, setUserIntl] = useLocalStorage<boolean | undefined>("intl", undefined, {initializeWithValue: false})
-
-  const onEvent = useCallback(
-    ({coords}: {coords: GeolocationCoordinates}) => {
-      const {latitude, longitude} = coords
-      setIsIntl(latitude < 24.5 || longitude > -52 || longitude < -170)
-    },
-    [setIsIntl]
-  )
-
-  useEffect(() => navigator.geolocation.getCurrentPosition(onEvent), [onEvent])
-  return [userIntl !== undefined ? userIntl : isIntl, setUserIntl]
+  return [userIntl || false, setUserIntl]
 }
 
 export default useIsInternational
