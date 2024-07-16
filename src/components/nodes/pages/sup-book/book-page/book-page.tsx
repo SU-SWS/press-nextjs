ae80bc8ea7b58d17d20af44f9eed7d451c56d2f5
rebeccahongsf <rebeccahongsf@gmail.com>
2024-07-16 12:29:38 -0700

    revert

--- src/components/nodes/pages/sup-book/book-page/book-page.tsx
+++ src/components/nodes/pages/sup-book/book-page/book-page.tsx
@@ -4,7 +4,7 @@ import {HTMLAttributes} from "react"
 import {Tab, TabPanel, Tabs, TabsList} from "@components/elements/tabs"
 import Wysiwyg from "@components/elements/wysiwyg"
 import {formatCurrency} from "@lib/utils/format-currency"
-import {ArrowLongLeftIcon, BookmarkIcon, ClipboardIcon, DocumentDuplicateIcon} from "@heroicons/react/24/outline"
+import {BookmarkIcon, ClipboardIcon, DocumentDuplicateIcon} from "@heroicons/react/24/outline"
 import Link from "@components/elements/link"
 import BookAwards from "@components/nodes/pages/sup-book/book-awards"
 import {getBookAncillaryContents} from "@lib/gql/gql-queries"
@@ -42,13 +42,12 @@ const BookPage = async ({node, ...props}: Props) => {
 
   const bookSubject = node.supBookSubjects && createLinkParams(node.supBookSubjects[0])
   return (
-    <div className="centered flex flex-col">
-      <article className="centered order-last" {...props}>
-        <>
-          <div className="mb-20 flex flex-col md:flex-row md:gap-24 md:pt-32">
-            <div className="relative left-1/2 flex w-screen -translate-x-1/2 flex-row justify-center gap-24 bg-fog-light px-20 pt-32 md:hidden">
-              <div className="mb-16 hidden w-8/12 flex-col sm:flex md:hidden">
-                <H1 className="type-3 mb-0">{node.title}</H1>
+    <article className="centered" {...props}>
+      <>
+        <div className="mb-20 flex flex-col md:flex-row md:gap-24 md:pt-32">
+          <div className="relative left-1/2 flex w-screen -translate-x-1/2 flex-row justify-center gap-24 bg-fog-light px-20 pt-32 md:hidden">
+            <div className="mb-16 hidden w-8/12 flex-col sm:flex md:hidden">
+              <H1 className="type-3 mb-0">{node.title}</H1>
 
               {node.supBookSubjects && (
                 <a
@@ -59,21 +58,21 @@ const BookPage = async ({node, ...props}: Props) => {
                 </a>
               )}
 
-                {node.supBookSubtitle && <div className="type-2 mt-5 font-medium">{node.supBookSubtitle}</div>}
+              {node.supBookSubtitle && <div className="type-2 mt-5 font-medium">{node.supBookSubtitle}</div>}
 
-                {node.supBookAuthorsFull && (
-                  <div className="type-1 mt-5 text-press-sand-dark">{node.supBookAuthorsFull}</div>
-                )}
-              </div>
-              <div className="order-first w-5/12">
-                <BookPageImage node={node} />
-              </div>
+              {node.supBookAuthorsFull && (
+                <div className="type-1 mt-5 text-press-sand-dark">{node.supBookAuthorsFull}</div>
+              )}
             </div>
-            <div className="flex flex-col md:w-2/3 md:gap-24 lg:flex-row">
-              <div className="lg:w-5/8">
-                <div className="rs-mb-0 rs-pb-3 flex flex-col border-b-2 border-fog">
-                  <div className="mt-7 flex flex-col sm:mt-0 sm:hidden md:flex">
-                    <H1 className="type-3 mb-0">{node.title}</H1>
+            <div className="order-first w-5/12">
+              <BookPageImage node={node} />
+            </div>
+          </div>
+          <div className="flex flex-col md:w-2/3 md:gap-24 lg:flex-row">
+            <div className="lg:w-5/8">
+              <div className="rs-mb-0 rs-pb-3 flex flex-col border-b-2 border-fog">
+                <div className="mt-7 flex flex-col sm:mt-0 sm:hidden md:flex">
+                  <H1 className="type-3 mb-0">{node.title}</H1>
 
                   {node.supBookSubjects && (
                     <a
@@ -84,23 +83,10 @@ const BookPage = async ({node, ...props}: Props) => {
                     </a>
                   )}
 
-                    {node.supBookAuthorsFull && (
-                      <div className="type-1 mt-5 text-press-sand-dark">{node.supBookAuthorsFull}</div>
-                    )}
-                  </div>
+                  {node.supBookSubtitle && <div className="type-2 mt-5 font-medium">{node.supBookSubtitle}</div>}
 
-                  {awards && (
-                    <div className="rs-mt-1 border-t-2 border-fog">
-                      <H2 className="flex w-fit items-center gap-2 bg-fog p-3 -text-m1 font-semibold">
-                        <BookmarkIcon width={20} className="fill-archway" />
-                        Award Winner
-                      </H2>
-                      <BookAwards>
-                        {awards.map(award => (
-                          <div key={award.id}>{award.name}</div>
-                        ))}
-                      </BookAwards>
-                    </div>
+                  {node.supBookAuthorsFull && (
+                    <div className="type-1 mt-5 text-press-sand-dark">{node.supBookAuthorsFull}</div>
                   )}
                 </div>
 
@@ -126,14 +112,9 @@ const BookPage = async ({node, ...props}: Props) => {
                   </div>
                 )}
 
-                  {node.supBookPubDateCloth?.time && (
-                    <div className="-text-m1 text-press-sand-dark">
-                      {new Date(node.supBookPubDateCloth.time).toLocaleDateString("en-us", {
-                        month: "long",
-                        year: "numeric",
-                      })}
-                    </div>
-                  )}
+                {node.supBookCopublisherName && (
+                  <div className="text-press-sand-dark">{node.supBookCopublisherName}</div>
+                )}
 
                 {node.supBookPubDateCloth?.time && (
                   <div className="-type-2 text-press-sand-dark">
@@ -173,9 +154,21 @@ const BookPage = async ({node, ...props}: Props) => {
               </div>
             </div>
 
-            <div className="md:order-first md:w-1/3">
-              <div className="hidden md:block">
-                <BookPageImage node={node} />
+            <div className="lg:w-3/8">
+              <div className="rs-mb-1 rs-pb-1 border-b-2 border-fog">
+                <BookPrecart
+                  bookTitle={node.title}
+                  usClothPrice={node.supBookPriceCloth}
+                  usClothSalePrice={node.supBookClothSalePrice}
+                  usClothSaleDiscount={node.supBookClothSalePercent}
+                  usPaperPrice={node.supBookPricePaper}
+                  usPaperSalePrice={node.supBookPaperSalePrice}
+                  usPaperSaleDiscount={node.supBookPaperSalePercent}
+                  usDigitalPrice={node.supBookPriceDigital}
+                  clothIsbn={node.supBookIsbn13Cloth}
+                  paperIsbn={node.supBookIsbn13Paper}
+                  digitalIsbn={node.supBookIsbn13Digital}
+                />
               </div>
 
               {node.supBookERetailers && (
@@ -204,56 +197,50 @@ const BookPage = async ({node, ...props}: Props) => {
               </Link>
             </div>
           </div>
-        </>
 
-        {(node.supBookDescription?.processed || node.supBookReviews || node.supBookAuthorInfo) && (
-          <Tabs className="mb-20 border-b border-fog pb-20">
-            <div className="mb-20 border-b border-fog">
-              <TabsList className="mx-auto max-w-5xl">
-                {node.supBookDescription?.processed && <Tab className="p-10">Description</Tab>}
-                {node.supBookReviews && <Tab className="p-10">Reviews</Tab>}
-                {node.supBookAuthorInfo && <Tab className="p-10">About the Author</Tab>}
-              </TabsList>
+          <div className="md:order-first md:w-1/3">
+            <div className="hidden md:block">
+              <BookPageImage node={node} />
             </div>
-            <div className="mx-auto max-w-5xl">
-              {node.supBookDescription?.processed && (
-                <TabPanel>
-                  <Wysiwyg html={node.supBookDescription?.processed} />
-                </TabPanel>
-              )}
-              {node.supBookReviews && (
-                <TabPanel>
-                  <Wysiwyg html={node.supBookReviews.processed} />
-                </TabPanel>
-              )}
-              {node.supBookAuthorInfo && (
-                <TabPanel>
-                  <Wysiwyg html={node.supBookAuthorInfo.processed} />
-                </TabPanel>
-              )}
-            </div>
-          </Tabs>
-        )}
 
-        {node.supBookSubjects && (
+            {hasExcerptAndMore && (
+              <Link
+                href={`${node.path}/excerpts`}
+                className="group rs-py-1 rs-mt-2 rs-px-2 mx-auto flex w-fit items-center justify-center gap-5 border-2 border-press-sand font-normal text-stone-dark no-underline hocus:border-cardinal-red hocus:bg-cardinal-red hocus:text-white hocus:underline md:mt-0"
+              >
+                <span>Excerpts + more</span>
+                <DocumentDuplicateIcon width={28} className="text-stone group-hocus:text-white" />
+              </Link>
+            )}
+          </div>
+        </div>
+      </>
+
+      {(node.supBookDescription?.processed || node.supBookReviews || node.supBookAuthorInfo) && (
+        <Tabs className="mb-20 border-b border-fog pb-20">
+          <div className="mb-20 border-b border-fog">
+            <TabsList className="mx-auto max-w-5xl">
+              {node.supBookDescription?.processed && <Tab className="p-10">Description</Tab>}
+              {node.supBookReviews && <Tab className="p-10">Reviews</Tab>}
+              {node.supBookAuthorInfo && <Tab className="p-10">About the Author</Tab>}
+            </TabsList>
+          </div>
           <div className="mx-auto max-w-5xl">
-            <H2 className="text-m1 font-bold">Related Subjects</H2>
-            <ul className="list-unstyled flex flex-col md:flex-row md:flex-wrap">
-              {node.supBookSubjects.map(subject => {
-                const linkParamsString = createLinkParams(subject)
-                return (
-                  <li key={subject.id} className="min-w-fit flex-1">
-                    <a
-                      href={`/search?${linkParamsString}`}
-                      className="-text-m1 font-normal text-stone-dark decoration-fog-dark underline-offset-[5px] hocus:text-archway-dark hocus:decoration-archway-dark hocus:decoration-2"
-                    >
-                      {subject.parent?.name && `${subject.parent.name} / `}
-                      {subject.name}
-                    </a>
-                  </li>
-                )
-              })}
-            </ul>
+            {node.supBookDescription?.processed && (
+              <TabPanel>
+                <Wysiwyg html={node.supBookDescription?.processed} />
+              </TabPanel>
+            )}
+            {node.supBookReviews && (
+              <TabPanel>
+                <Wysiwyg html={node.supBookReviews.processed} />
+              </TabPanel>
+            )}
+            {node.supBookAuthorInfo && (
+              <TabPanel>
+                <Wysiwyg html={node.supBookAuthorInfo.processed} />
+              </TabPanel>
+            )}
           </div>
         </Tabs>
       )}
