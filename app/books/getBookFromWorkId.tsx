c77a4c659e4b253c98479b9bd9c49e00bacc498e
Mike Decker <pookmish@gmail.com>
2024-07-17 10:55:35 -0700

    Use views for redirecting legacy pages

--- app/books/getBookFromWorkId.tsx
+++ app/books/getBookFromWorkId.tsx
@@ -1,18 +1,15 @@
-import {BooksQuery, NodeSupBook} from "@lib/gql/__generated__/drupal"
+import {NodeSupBook} from "@lib/gql/__generated__/drupal"
 import {graphqlClient} from "@lib/gql/gql-client"
 
 export const getBookFromWorkId = async (workId: number): Promise<NodeSupBook | undefined> => {
-  let fetchMore = true
-  let query: BooksQuery
-  let afterCursor = null
-  let desiredNode: NodeSupBook | undefined
-  while (fetchMore) {
-    query = await graphqlClient({next: {tags: ["views:sup_books"]}}).Books({after: afterCursor})
-
-    desiredNode = query.nodeSupBooks.nodes.find(node => node.supBookWorkIdNumber === workId) as NodeSupBook | undefined
-
-    fetchMore = query.nodeSupBooks.pageInfo.hasNextPage && !desiredNode
-    afterCursor = query.nodeSupBooks.pageInfo.endCursor
+  const filters = {
+    term_node_taxonomy_name_depth: '""',
+    term_node_taxonomy_name_depth_2: '""',
+    term_node_taxonomy_name_depth_1: '""',
+    term_node_taxonomy_name_depth_3: '""',
+    sup_book_work_id_number_value: `${workId}`,
   }
-  return desiredNode
+
+  const graphqlResponse = await graphqlClient().supBooks({filters})
+  return graphqlResponse.supBooksView?.results.pop() as NodeSupBook
 }
