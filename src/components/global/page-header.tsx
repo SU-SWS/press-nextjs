aad502a6b8092ef96e822acd07fa0f15e77e5d02
rebeccahongsf <rebeccahongsf@gmail.com>
2024-07-18 15:42:40 -0700

    SUP-256 | fixup masthead responsive sizing

--- src/components/global/page-header.tsx
+++ src/components/global/page-header.tsx
@@ -15,7 +15,7 @@ const PageHeader = async () => {
       {globalMessageConfig && <GlobalMessage {...globalMessageConfig} />}
       <div className="min-h-50 centered pr-24 lg:pr-0">
         <div className="flex w-full items-center justify-between gap-20">
-          <div className="rs-pt-2 rs-pb-3">
+          <div className="rs-pt-1 rs-pb-2 lg:rs-pt-2 lg:rs-pb-3">
             <Link
               href="/"
               className="flex flex-col gap-4 no-underline lg:flex-row"
