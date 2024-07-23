12d2eb9263ae544f9656d1be70c65cb70ee87abf
Mike Decker <pookmish@gmail.com>
2024-07-23 13:57:31 -0700

    Refactor related books to use rest api instead of instantsearch

--- src/components/algolia-search/algolia-related-books.tsx
+++ src/components/algolia-search/algolia-related-books.tsx
@@ -1,20 +1,113 @@
-import {getConfigPage} from "@lib/gql/gql-queries"
-import {NodeSupBook, StanfordBasicSiteSetting} from "@lib/gql/__generated__/drupal"
-import RelatedBooks from "@components/algolia-search/algolia-related-books-client"
+import {NodeSupBook} from "@lib/gql/__generated__/drupal"
+import type {RecommendResults} from "algoliasearch-helper"
+import {unstable_cache as nextCache} from "next/cache"
+import {BookHit} from "@components/algolia-search/hits/sup-book"
+import {H2, H3} from "@components/elements/headers"
+import Image from "next/image"
+import {BookmarkIcon} from "@heroicons/react/24/outline"
+import Link from "@components/elements/link"
+
+const getRelatedContent = nextCache(
+  async (objectID: string): Promise<BookHit[]> => {
+    const appID = process.env.ALGOLIA_ID
+    const indexName = process.env.ALGOLIA_INDEX
+    const apiKey = process.env.ALGOLIA_KEY
+    if (!appID || !indexName || !apiKey) return []
+
+    const options: RequestInit = {
+      method: "POST",
+      cache: "no-store",
+      headers: {
+        "Content-Type": "application/json",
+        "X-Algolia-Application-Id": appID,
+        "X-Algolia-API-Key": apiKey,
+      },
+      body: JSON.stringify({
+        requests: [
+          {
+            indexName: indexName,
+            model: "related-products",
+            maxRecommendations: 5,
+            threshold: 0,
+            objectID,
+            queryParameters: {
+              filters: "book_type:book",
+            },
+          },
+        ],
+      }),
+    }
+
+    const recommendations: {results: RecommendResults<BookHit>["_rawResults"]} = await fetch(
+      `https://${appID}-dsn.algolia.net/1/indexes/*/recommendations`,
+      options
+    ).then(res => res.json())
+
+    const hits = recommendations?.results?.[0].hits || []
+    hits.map(hit => {
+      delete hit._highlightResult
+      delete hit._snippetResult
+    })
+    return hits
+  },
+  ["related-books"],
+  {revalidate: 2592000, tags: ["related-books"]}
+)
 
 const AlgoliaRelatedBooks = async ({objectId}: {objectId: NodeSupBook["id"]}) => {
-  const siteSettings = await getConfigPage<StanfordBasicSiteSetting>("StanfordBasicSiteSetting")
-
-  if (!siteSettings?.suSiteAlgoliaId || !siteSettings.suSiteAlgoliaIndex || !siteSettings.suSiteAlgoliaSearch || true)
-    return
+  const recommendations = await getRelatedContent(objectId)
+  if (recommendations.length === 0) return
 
   return (
-    <RelatedBooks
-      objectId={objectId}
-      appId={siteSettings?.suSiteAlgoliaId}
-      searchIndex={siteSettings?.suSiteAlgoliaIndex}
-      searchApiKey={siteSettings?.suSiteAlgoliaSearch}
-    />
+    <section aria-labelledby={`${objectId}-related`} className="centered mt-64 border-t-2 border-press-sand-dark">
+      <H2 id={`${objectId}-related`} className="mb-32 mt-16 font-medium text-stone-dark">
+        Explore more
+      </H2>
+      <ul className="list-unstyled mx-auto grid w-11/12 grid-cols-1 gap-10 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5">
+        {recommendations.map(rec => (
+          <RelatedBook key={rec.objectID} item={rec} />
+        ))}
+      </ul>
+    </section>
+  )
+}
+
+const RelatedBook = ({item}: {item: BookHit}) => {
+  const imageUrl =
+    item.photo && item.photo.replace(new URL(item.photo).origin, process.env.NEXT_PUBLIC_DRUPAL_BASE_URL as string)
+  return (
+    <li className="mx-auto max-w-3xl">
+      <div className="relative">
+        <div className="rs-mb-1 relative aspect-[2/3] w-full">
+          <Image
+            className="ed11y-ignore object-cover"
+            src={imageUrl || "/default-book-image.jpg"}
+            alt=""
+            fill
+            sizes="400px"
+          />
+          {item.book_award_winner && (
+            <div className="absolute left-5 top-0 flex max-w-[90%] items-center gap-3 bg-fog py-2 pl-3 pr-5">
+              <BookmarkIcon width={20} className="fill-stone-dark" />
+              Award winner
+            </div>
+          )}
+        </div>
+
+        <H3 className="type-0 mb-5 font-normal">
+          <Link
+            className="stretched-link font-medium text-stone-dark"
+            href={item.url.replace(new URL(item.url).origin, "")}
+          >
+            {item.title}
+          </Link>
+        </H3>
+      </div>
+
+      {item.book_subtitle && <div className="rs-mb-3 text-press-sand-dark">{item.book_subtitle}</div>}
+
+      {item.book_authors && <div className="mb-0 text-press-sand-dark">{item.book_authors}</div>}
+    </li>
   )
 }
 
