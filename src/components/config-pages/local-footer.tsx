77437c5ad0a4ba04eceb1478c469d636bf2fb1c1
rebeccahongsf <rebeccahongsf@gmail.com>
2024-07-26 10:06:45 -0700

    SUP-263 | tweak logo and social media spacing in footer

--- src/components/config-pages/local-footer.tsx
+++ src/components/config-pages/local-footer.tsx
@@ -15,8 +15,8 @@ const LocalFooter = async () => {
 
   return (
     <div className="rs-py-6 local-footer bg-stone-dark">
-      <div className="rs-mb-6 centered">
-        <div className="rs-mb-6">
+      <div className="centered">
+        <div className="rs-mb-3">
           <FooterLockup />
         </div>
 
