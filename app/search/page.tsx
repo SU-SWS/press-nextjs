0090ff701ab68624d9afeef0e998ba0b8ea9134e
Rebecca Hong <34019925+rebeccahongsf@users.noreply.github.com>
2024-08-27 09:17:35 -0700

    SUP-289 | adjust h1 spacing (#63)
    * SUP-289 | adjust h1 spacing
    
    * Update page.tsx
    
    ---------
    
    Co-authored-by: pookmish <pookmish@gmail.com>

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -35,8 +35,8 @@ const Page = async ({searchParams}: {searchParams?: {[_key: string]: string}}) =
   return (
     <div className="centered mt-32">
       <div className="mx-auto 3xl:w-10/12">
-        <H1 className="mb-44" id="page-title">
-          Search
+        <H1 className="rs-mb-2" id="page-title">
+          Search Our Site
         </H1>
 
         {appId && indexName && initialState && apiKey && (
