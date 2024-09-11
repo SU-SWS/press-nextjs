8a0cd1f421616946ea80a667b6806d4493bf4f9c
Mike Decker <pookmish@stanford.edu>
2024-09-11 11:02:17 -0700

    Adjust excerpt page to use sidenav of other excerpts

--- src/components/nodes/pages/sup-book-ancilary/sup-book-ancillary-page.tsx
+++ src/components/nodes/pages/sup-book-ancilary/sup-book-ancillary-page.tsx
@@ -1,4 +1,4 @@
-import {NodeSupBookAncillary} from "@lib/gql/__generated__/drupal"
+import {MenuItem as MenuItemType, NodeSupBookAncillary} from "@lib/gql/__generated__/drupal"
 import {H1} from "@components/elements/headers"
 import {HTMLAttributes} from "react"
 import Rows from "@components/paragraphs/rows/rows"
@@ -6,6 +6,8 @@ import BookPageImage from "@components/nodes/pages/sup-book/book-page-image"
 import BackToLink from "@components/elements/back-to-link"
 import {twMerge} from "tailwind-merge"
 import Wysiwyg from "@components/elements/wysiwyg"
+import SideNav from "@components/menu/side-nav"
+import {getBookAncillaryContents} from "@lib/gql/gql-queries"
 
 type Props = HTMLAttributes<HTMLElement> & {
   node: NodeSupBookAncillary
@@ -13,6 +15,19 @@ type Props = HTMLAttributes<HTMLElement> & {
 
 const SupBookAncillaryPage = async ({node, ...props}: Props) => {
   const book = node.supAncillaryBook
+
+  const ancillaryPages = await getBookAncillaryContents(node.supAncillaryBook.id, node.path)
+  const ancillarryMenuItems: MenuItemType[] = ancillaryPages.map(page => ({
+    id: page.id,
+    children: [],
+    url: page.path,
+    title: page.title,
+    attributes: {},
+    expanded: true,
+    internal: true,
+    langcode: {},
+  }))
+
   return (
     <BackToLink
       {...props}
@@ -20,34 +35,43 @@ const SupBookAncillaryPage = async ({node, ...props}: Props) => {
       className={twMerge("centered", props.className)}
       linkClasses="flex w-fit items-center gap-5"
       title="Excerpts + More"
-      childrenProps={{className: "rs-mt-4 mx-auto max-w-900 gap-10"}}
+      childrenProps={{className: "centered flex gap-[17.1rem]"}}
       isArticle
     >
-      <H1 className="type-3 xl:text-[4.1rem]">
+      <H1 className="sr-only">
         {node.title} for <em>{book.title}</em>
       </H1>
-      <div className="rs-mb-0 flex flex-col md:flex-row">
-        <div className="rs-ml-1 flex flex-col">
-          {node.supAncillaryBook.supBookSubtitle && (
-            <div className="type-1 font-medium xl:text-26">{node.supAncillaryBook.supBookSubtitle}</div>
-          )}
 
-          {node.supAncillaryBook.supBookAuthorsFull && (
-            <div className="type-0 mt-5 text-press-sand-dark xl:text-21">
-              {node.supAncillaryBook.supBookAuthorsFull}
-            </div>
-          )}
-        </div>
-
+      <aside className="hidden w-1/4 shrink-0 lg:block">
+        <a href="#page-content" className="skiplink">
+          Skip secondary navigation
+        </a>
         {node.supAncillaryBook.supBookImage?.mediaImage && (
           <div className="relative order-first w-full max-w-[16rem] shrink-0">
             <BookPageImage node={node.supAncillaryBook} />
           </div>
         )}
-      </div>
+        <SideNav menuItems={ancillarryMenuItems} activeTrail={[node.id]} />
+      </aside>
 
-      <Wysiwyg html={node.body?.processed} />
-      <Rows components={node.supAncillaryParagraphs} className="px-5" />
+      <div id="page-content" className="flex-grow">
+        <div className="rs-mb-0 flex flex-col md:flex-row">
+          <div className="rs-ml-1 flex flex-col">
+            {node.supAncillaryBook.supBookSubtitle && (
+              <div className="type-1 font-medium xl:text-26">{node.supAncillaryBook.supBookSubtitle}</div>
+            )}
+
+            {node.supAncillaryBook.supBookAuthorsFull && (
+              <div className="type-0 mt-5 text-press-sand-dark xl:text-21">
+                {node.supAncillaryBook.supBookAuthorsFull}
+              </div>
+            )}
+          </div>
+        </div>
+
+        <Wysiwyg html={node.body?.processed} />
+        <Rows components={node.supAncillaryParagraphs} className="px-5" />
+      </div>
     </BackToLink>
   )
 }
