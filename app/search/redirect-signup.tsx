c9b6010e473d34ab68da24663279f7e60f4e7cc7
Mike Decker <pookmish@stanford.edu>
2025-05-16 14:25:23 -0700

    use router

--- app/search/redirect-signup.tsx
+++ app/search/redirect-signup.tsx
@@ -2,17 +2,19 @@
 
 import {useCountdown} from "usehooks-ts"
 import {useEffect} from "react"
+import {useRouter} from "next/navigation";
 
 const RedirectSignup = () => {
   const [count, {startCountdown}] = useCountdown({
     countStart: 5,
     intervalMs: 1000,
   })
+  const router = useRouter()
 
   useEffect(() => startCountdown(), [startCountdown])
   useEffect(() => {
-    if (count === 0) window.location.href = "https://signup.e2ma.net/signup/1414766/1406582/"
-  }, [count])
+    if (count === 0) router.replace("https://signup.e2ma.net/signup/1414766/1406582/")
+  }, [count, router])
   return <p>Redirecting to Newsletter Signup form in {count} seconds</p>
 }
 export default RedirectSignup
