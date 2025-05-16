f2bec519243d0f0bb2eba6ed45eaab9cc24e77bb
Mike Decker <pookmish@stanford.edu>
2025-05-16 14:39:53 -0700

    added metatag

--- app/email-signup/page.tsx
+++ app/email-signup/page.tsx
@@ -1,16 +1,17 @@
 import {H1} from "@components/elements/headers"
-import RedirectSignup from "../search/redirect-signup"
+import RedirectSignup from "./redirect-signup"
 
 const Page = () => {
   return (
-    <div className="centered mt-32">
+    <article className="centered mt-32">
+      <title>Newsletter Signup | Stanford University Press</title>
       <div className="mx-auto 3xl:w-10/12">
         <H1 className="rs-mb-2" id="page-title">
           Newsletter Signup
         </H1>
         <RedirectSignup />
       </div>
-    </div>
+    </article>
   )
 }
 
