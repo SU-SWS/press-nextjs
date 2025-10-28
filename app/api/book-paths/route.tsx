89336bc4c8ab63ca28abc78bc1c30797cb069eab
Mike Decker <pookmish@stanford.edu>
2025-10-28 10:25:26 -0700

    Added cron job to clear legacy book paths

--- app/api/book-paths/route.tsx
+++ app/api/book-paths/route.tsx
@@ -1,7 +1,7 @@
 import {NextResponse} from "next/server"
 import {getLegacyBookPaths} from "@lib/utils/getLegacyBookPaths"
 
-export const revalidate = 604800
+export const revalidate = 31536000
 export const maxDuration = 60
 
 export const GET = async () => {
