578e23eb2f7e76d7b73db2dd57cad93fe7f551ad
Mike Decker <pookmish@stanford.edu>
2026-06-11 11:15:36 -0700

    Fix legacy page builds

--- app/books/legacy/[id]/page.tsx
+++ app/books/legacy/[id]/page.tsx
@@ -11,7 +11,7 @@ const LegacyBookPage = async (props: {params: Promise<{id: string}>}) => {
 }
 
 export const generateStaticParams = async (): Promise<Array<{id: string}>> => {
-  if (process.env.VERCEL_ENV === "preview") return [{id: ""}]
+  if (process.env.VERCEL_ENV === "preview") return [{id: "1"}]
   const params = await getLegacyBookPaths()
   return params.map(item => ({id: item.uuid.toString()})).slice(0, 100)
 }
