1df2a14e428afca3d7844a8e68f5a0c0a3fc9dc0
Mike Decker <pookmish@stanford.edu>
2026-09-11 15:02:24 -0700

    allow search parameters on images

--- next.config.ts
+++ next.config.ts
@@ -39,7 +39,6 @@ const nextConfig: NextConfig = {
         protocol: drupalUrl.protocol.replace(":", "") === "http" ? "http" : "https",
         hostname: drupalUrl.hostname,
         pathname: "/sites/**",
-        search: "",
       },
       {
         protocol: "https",
